60dc13d8b3bcdc5a15510e2726aef08e44966be1
nareshannavaram <annavaramnaresh111@gmail.com>
2022-01-21 15:19:12 +0530

    first commit

---
+++ Demo1/src/pack1/Class1.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class Class1 {
+	
+	int a= 76;
+	
+	
+	public static void main(String[] args) {
+		
+		Class1 obje=new Class1();
+		System.out.println(obje.a);
+	}
+}
---
+++ hellonaresh/src/hello/hai.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class hai {
+	public static void main(String[] args) {
+		System.out.println("fggfkjhJ");
+	}
+
+}
